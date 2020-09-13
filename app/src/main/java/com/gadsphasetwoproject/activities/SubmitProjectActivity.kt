9c396df6d71eeb121154c4a318fc6c46a88cc37c
timmyCoder <olatoyedan@gmail.com>
2020-09-13 17:47:16 +0100

    done the submission of form

--- app/src/main/java/com/gadsphasetwoproject/activities/SubmitProjectActivity.kt
+++ app/src/main/java/com/gadsphasetwoproject/activities/SubmitProjectActivity.kt
@@ -5,27 +5,110 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Window
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.gadsphasetwoproject.R
 import com.gadsphasetwoproject.databinding.ActivitySubmitProjectBinding
+import com.gadsphasetwoproject.networkCalls.ApiInterface
+import com.gadsphasetwoproject.utils.CustomProgressDialog
+import com.gadsphasetwoproject.utils.Utils
 import es.dmoral.toasty.Toasty
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import timber.log.Timber
 
 class SubmitProjectActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySubmitProjectBinding
+
+    private lateinit var progressDialog: CustomProgressDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_submit_project)
         setSupportActionBar(findViewById(R.id.submit_toolbar))
+        progressDialog = CustomProgressDialog(this)
         with(binding) {
             backButton.setOnClickListener {
                 onBackPressed()
             }
 
             btnSaveAccount.setOnClickListener {
-                showSuccessfulDialog()
+                if (Utils.isOnline(this@SubmitProjectActivity)) {
+                    if (!validateFields()) {
+                        return@setOnClickListener
+                    }
+                    showConfirmationDialog()
+                }
             }
         }
+    }
+
+    private fun showConfirmationDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        dialog.setContentView(R.layout.confirmation_dialog)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setCancelable(true)
+        val cancelBtn: ImageView = dialog.findViewById(R.id.cancelBtn)
+        cancelBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        val yesBtn: Button = dialog.findViewById(R.id.submit_button)
+        yesBtn.setOnClickListener {
+            submitProject()
+            dialog.dismiss()
+        }
+
+        dialog.show()
+
+    }
+
+    private fun submitProject() {
+        progressDialog.showDialog()
+        val apiInterface = ApiInterface.submit().submitProject(
+            binding.edtxtEmail.text.toString(),
+            binding.edtxtFirstName.text.toString(),
+            binding.edtxtLastName.text.toString(),
+            binding.edtxtGithubLink.text.toString()
+        )
+
+        apiInterface.enqueue(object : Callback<Void> {
+            override fun onResponse(call: Call<Void>?, response: Response<Void>?) {
+                if (response!!.isSuccessful) {
+                    progressDialog.hideDialog()
+                    Timber.d("${response.code()}")
+                    Toasty.success(
+                        this@SubmitProjectActivity,
+                        "successfully sent",
+                        Toasty.LENGTH_SHORT,
+                        true
+                    ).show()
+                    showSuccessfulDialog()
+                }
+            }
+
+            override fun onFailure(call: Call<Void>?, t: Throwable?) {
+                progressDialog.hideDialog()
+                showUnSuccessfulDialog()
+            }
+        })
+
+    }
+
+    private fun showUnSuccessfulDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        dialog.setContentView(R.layout.submission_not_successful_dialog)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setCancelable(true)
+
+        dialog.show()
+
     }
 
     private fun showSuccessfulDialog() {
--- app/src/main/java/com/gadsphasetwoproject/networkCalls/ApiInterface.kt
+++ app/src/main/java/com/gadsphasetwoproject/networkCalls/ApiInterface.kt
@@ -5,7 +5,11 @@ import com.gadsphasetwoproject.model.UserIq
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
+
 
 interface ApiInterface {
 
@@ -16,9 +20,19 @@ interface ApiInterface {
     @GET("/api/skilliq")
     fun getLearnersIq(): Call<List<UserIq>>
 
+    @POST("1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
+    @FormUrlEncoded
+    fun submitProject(
+        @Field("entry.1824927963") email: String?,
+        @Field("entry.1877115667") name: String?,
+        @Field("entry.2006916086") lastName: String?,
+        @Field("entry.284483984") link: String?
+    ): Call<Void>
+
     companion object {
 
         var BASE_URL = "https://gadsapi.herokuapp.com/"
+        var FORM_BASE_URL = "https://docs.google.com/forms/d/e/"
 
         fun create(): ApiInterface {
 
@@ -28,6 +42,14 @@ interface ApiInterface {
                 .build()
             return retrofit.create(ApiInterface::class.java)
 
+        }
+
+        fun submit(): ApiInterface {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(FORM_BASE_URL)
+                .build()
+            return retrofit.create(ApiInterface::class.java)
         }
     }
 }
