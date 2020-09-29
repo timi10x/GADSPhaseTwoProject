336e782227a9278ce75357a9a6f5394e790ff450
timmyCoder <olatoyedan@gmail.com>
2020-09-29 18:34:34 +0100

    live data for submit project view model done

--- app/src/androidTest/java/com/gadsphasetwo/viewmodel/SubmitProjectViewModelTest.kt
+++ app/src/androidTest/java/com/gadsphasetwo/viewmodel/SubmitProjectViewModelTest.kt
@@ -1,10 +1,12 @@
 package com.gadsphasetwo.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.gadsphasetwo.getOrAwaitValue
 import com.gadsphasetwoproject.model.viewModel.SubmitProjectViewModel
 import com.gadsphasetwoproject.room.repository.SubmitProjectRepository
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -35,6 +37,7 @@ class SubmitProjectViewModelTest {
     @Test
     fun testViewModel() {
         /*submitProjectViewModel.submitProject()*/
+        assertEquals(submitProjectViewModel.getLoading.getOrAwaitValue(), true)
     }
 
 }
